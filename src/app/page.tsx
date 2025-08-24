4b94f284aa1a430f9625da1af23cef25352aac2e
Abdul Razak <moabdulrazak17@gmail.com>
2025-08-24 01:37:32 +0000

    feat: setup layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { ModeToggle } from "@/components/layouts/mode-toggle";
+import { Shell } from "@/components/shell";
+
 export default function Home() {
   return (
-    <div className="font-bold text-3xl underline">
-      <h1>Hi Mom!</h1>
-    </div>
+    <Shell className="flex h-dvh flex-col items-center justify-center">
+      <section className="prose prose-zinc dark:prose-invert flex flex-col items-center text-center font-mono">
+        <h1 className="font-bold text-4xl">Ecommerce App</h1>
+        <ModeToggle />
+      </section>
+    </Shell>
   );
 }
